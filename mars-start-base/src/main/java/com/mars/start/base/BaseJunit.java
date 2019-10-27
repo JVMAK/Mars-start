4436a95d2718c423e4199b34c378a3620a7b14a5
yuye <631941527@qq.com>
2019-10-27 18:53:56 +0800

    update version

---
+++ mars-start-base/src/main/java/com/mars/start/base/BaseJunit.java
@@ -0,0 +1,44 @@
+package com.mars.start.base;
+
+import com.mars.core.annotation.MarsTest;
+import com.mars.jdbc.base.BaseInitJdbc;
+import com.mars.start.startmap.StartLoadList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 单测基类
+ */
+public abstract class BaseJunit {
+
+    private Logger logger = LoggerFactory.getLogger(BaseJunit.class);
+
+    public BaseJunit(){
+        MarsTest marsTest = this.getClass().getAnnotation(MarsTest.class);
+        if(marsTest == null || marsTest.testClass() == null){
+            logger.error("没有正确的配置MarsTest注解");
+        } else {
+            init(marsTest.testClass(),marsTest.config());
+        }
+    }
+
+    /**
+     * 加载单测需要的资源
+     * @param packName 包
+     * @param suffix 后缀
+     */
+    public void init(Class packName,String suffix){
+        if(suffix != null && suffix.equals("")){
+            suffix = null;
+        }
+
+        MarsJunitStart.setStartList(StartLoadList.initTestStartList());
+        MarsJunitStart.start(getBaseInitJdbc(),packName,this,suffix);
+    }
+
+    /**
+     * 获取加载jdbc的类
+     * @return 类
+     */
+    public abstract BaseInitJdbc getBaseInitJdbc();
+}
--- mars-start-pure/src/main/java/com/mars/junit/MarsJunit.java
+++ mars-start-pure/src/main/java/com/mars/junit/MarsJunit.java
@@ -1,33 +1,16 @@
 package com.mars.junit;
 
+import com.mars.jdbc.base.BaseInitJdbc;
 import com.mars.mj.init.InitJdbc;
-import com.mars.start.base.MarsJunitStart;
-import com.mars.start.startmap.StartLoadList;
+import com.mars.start.base.BaseJunit;
 
 /**
  * junit
  */
-public abstract class MarsJunit {
+public abstract class MarsJunit extends BaseJunit {
 
-    /**
-     * 加载项目启动的必要数据
-     * @param packName
-     */
-    public void init(Class packName,String suffix){
-        MarsJunitStart.setStartList(StartLoadList.initTestStartList());
-        MarsJunitStart.start(new InitJdbc(),packName,this,suffix);
+    @Override
+    public BaseInitJdbc getBaseInitJdbc() {
+        return new InitJdbc();
     }
-
-    /**
-     * 加载项目启动的必要数据
-     * @param packName
-     */
-    public void init(Class packName){
-        init(packName,null);
-    }
-
-    /**
-     * 单测开始前
-     */
-    public abstract void before();
 }
